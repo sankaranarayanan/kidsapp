02adeecaaa870522566b316f790aaae4c6bb8033
Sankar <sankar@absolut-e.com>
2011-10-20 15:05:16 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kidsapp::Application.config.secret_token = '4b84d7ed170b0f6447d2ae389327b30e67d682aaa0adbe39c169a79761e5332083f00c4a1e25a3627326eff6059ed9ba4f1f5a2c958c7c5bf21a0373312f139c'
